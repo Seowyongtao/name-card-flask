6a77092fb63bdeecda149b5cee4fadff08667790
Seow Yong Tao <seowyongtao22@gmail.com>
2021-10-22 18:01:35 +0800

    added html

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_route():
-    return "<h1>Hello</h1>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
